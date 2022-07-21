45db88c1a82b948235dae860637a2cbca68ec2a5
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-07-21 17:30:35 -0500

    working test

--- src/GitHubCard/GitHubCard.js
+++ src/GitHubCard/GitHubCard.js
@@ -1,18 +1,20 @@
 import React from "react"
 import Card from "react-bootstrap/Card"
-import MickeyMouse from './Mickey_Mouse.png'
+
 
 function GitHubCard() {
   return (
       <div className="card">
         <Card style={{ width: "18rem"}}>
-        {/* Use your GitHub info instead, but Mickey Mouse is used as an example */}
-        <Card.Img variant="top" src={MickeyMouse} />
-        {/* Source: https://en.wikipedia.org/wiki/Mickey_Mouse */}
+       
+        <Card.Img variant="top" src={`https://github.com/beck68725 `}/>
+        
         <Card.Body>
-            <Card.Title>Mickey Mouse</Card.Title>
+            <Card.Title>Rebecca Coburn</Card.Title>
+            <h1>Coding is great!!!</h1>
             <Card.Text>
-            I am a cartoon character and am one of the world's most recognizable fictional characters.
+            I am a coder!!
+
             </Card.Text>
         </Card.Body>
         </Card>
